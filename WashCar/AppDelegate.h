5a00bd40fbd05d20a5e57a1025e1220f6081aaa9
高 玉锋 <scp6600@nate.local>
2015-06-24 11:17:50 +0800

    Initial Commit

---
+++ WashCar/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  WashCar
+//
+//  Created by 高 玉锋 on 15/6/24.
+//  Copyright (c) 2015年 高 玉锋. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ WashCar/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  WashCar
+//
+//  Created by 高 玉锋 on 15/6/24.
+//  Copyright (c) 2015年 高 玉锋. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
